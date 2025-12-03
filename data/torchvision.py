e6da4fbbc7376be5923ab346dcb2ea6864096bf0
looZ149 <nklitsch@web.de>
2025-12-03 11:35:20 +0100

    Update
    img folder + .py update

--- data/torchvision.py
+++ data/torchvision.py
@@ -1,1 +1,8 @@
-from torchvision import transforms
+import torch
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+plt.rcParams["savefig.bbox"] = 'tight'  # Ensure tight bounding boxes for saved figures
+
+from torchvision.transforms import v2
+from torchvision.io import decode_image
